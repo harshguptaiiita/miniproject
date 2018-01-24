8ad2447c9cfab6be014cfc2b4abd1789265599cb
MEHUL <rit2015025@iita.ac.in>
2018-01-24 17:12:21 +0530

    changed

--- app/src/main/java/com/example/harshgupta/project/MainActivity.java
+++ app/src/main/java/com/example/harshgupta/project/MainActivity.java
@@ -1,15 +1,94 @@
 package com.example.harshgupta.project;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.speech.RecognizerIntent;
+import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Locale;
+
+import static java.lang.Thread.sleep;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextToSpeech tts;
+    String a="Welcome";
+    public ArrayList<String> result=null;
+    final int REQ_CODE_SPEECH_INPUT =100;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //back
-        //kaise ho tum
+        talk(a);
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        promptSpeechInput();
+    }
+    public void talk(final String spk){
+        tts = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            public void onInit(int status) {
+                if (status == TextToSpeech.SUCCESS) {
+                    tts.setLanguage(Locale.US);
+                    tts.speak(spk,TextToSpeech.QUEUE_FLUSH,null ); //What do I put here?
+                } else {
+                    Toast.makeText(getApplicationContext(),
+                            "Feature not Supported in Your Device",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+    public void onDestroy() {
+        // Don't forget to shutdown tts!
+        if (tts != null) {
+            tts.stop();
+            tts.shutdown();
+        }
+        super.onDestroy();
+    }
+    private void promptSpeechInput() {
+
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
+                getString(R.string.speech_prompt));
+        try {
+            startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
+        } catch (ActivityNotFoundException a) {
+            Toast.makeText(getApplicationContext(),
+                    getString(R.string.speech_not_supported),
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case REQ_CODE_SPEECH_INPUT: {
+                if (resultCode == RESULT_OK && null != data) {
+
+                    result = data
+                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    //txtSpeechInput.setText(result.get(0));
+                    Log.e("Result",result.get(0));
+                    if(result.get(0).equals("contacts")){
+                        Intent ab= new Intent(this,contacts.class);
+                        startActivity(ab);
+                    }
+                }
+                break;
+            }
+
+        }
+
     }
 }
